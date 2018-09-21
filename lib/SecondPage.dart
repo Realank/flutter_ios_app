be16482b29804d89e4d014af92a10a8d32c91d60
Liu Yanbo <yanbo.liu@ucarinc.com>
2018-09-21 17:40:06 +0800

    change second page style

--- lib/SecondPage.dart
+++ lib/SecondPage.dart
@@ -4,9 +4,28 @@ class SecondPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return CupertinoPageScaffold(
-        navigationBar: CupertinoNavigationBar(),
-        child: ListView.builder(itemBuilder: (context, index) {
-          return Text('cell $index');
-        }));
+      child: CustomScrollView(
+        slivers: <Widget>[
+          const CupertinoSliverNavigationBar(),
+          SliverPadding(
+            padding: MediaQuery.of(context)
+                .removePadding(
+                  removeTop: true,
+                  removeLeft: true,
+                  removeRight: true,
+                )
+                .padding,
+            sliver: SliverList(
+              delegate: SliverChildBuilderDelegate(
+                (BuildContext context, int index) {
+                  return Text('cell $index');
+                },
+                childCount: 50,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
